099a5f3b24a42568a1ad80a78c8624528611fb87
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2020-04-15 21:45:26 +0900

    Student_Management_System

---
+++ Student_Management_System/src/Ssh/Lecture.java
@@ -0,0 +1,15 @@
+package Ssh;
+
+import java.util.Scanner;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name = in.next();
+		System.out.println("°ú¸ñ¸í: " + name);
+		
+
+	}
+
+}
