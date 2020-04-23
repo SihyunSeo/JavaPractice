7c9aa77a54d2379152fb43c8c7ce3874da50c1c0
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2020-04-23 20:51:31 +0900

    put_class_professor_information

--- Student_Management_System/src/Ssh/Lecture.java
+++ Student_Management_System/src/Ssh/Lecture.java
@@ -1,10 +1,10 @@
 package Ssh;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Lecture {
 	//과목명, 담당교수
-	
 	String strName;            //과목명
 	String strProfessor;       //담당교수
 			
@@ -30,6 +30,33 @@ public class Lecture {
 	{
 		this.strProfessor = professor;
 	}
+	public static void main(String[] args)
+	{
+		
+		Scanner count = new Scanner(System.in);
+		System.out.print("수강하는 과목의 개수를 입력하시오: ");
+		int nameCount = count.nextInt();
+		
+		ArrayList<String> list1 = new ArrayList<>();
+		
+		for(int num = 0; num < nameCount; num++)
+		{
+			Scanner putName = new Scanner(System.in);
+			System.out.print("과목명을 입력하시오: ");
+			String name = putName.next();
 
+			
+			Scanner putProfessor = new Scanner(System.in);
+			System.out.print("담당교수를 입력하시오: ");
+			String professor = putProfessor.next();
+			list1.add(name+" : "+professor);
+		}
+		for(String i : list1) 
+		{
+			System.out.println(i);
+		}
+		
+
+	}
 
 }
