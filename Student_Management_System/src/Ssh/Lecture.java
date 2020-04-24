ee0183a36daf578619c2a542217cb2472765c056
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2020-04-24 21:50:16 +0900

    edit SMS test ver

--- Student_Management_System/src/Ssh/Lecture.java
+++ Student_Management_System/src/Ssh/Lecture.java
@@ -1,62 +1,36 @@
 package Ssh;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-
-public class Lecture {
-	//과목명, 담당교수
-	String strName;            //과목명
-	String strProfessor;       //담당교수
-			
-	public Lecture(String name, String professor)  //생성자
-	{
-		this.strName = name;
-		this.strProfessor = professor;
-	}
-	
-	String getName() //접근자
-	{
-		return strName;
-	}
-	void setName(String name) //설정자
-	{
-		this.strName = name;
-	}
-	String getProfessor() //접근자
-	{
-		return strProfessor;
-	}
-	void setProfessor(String professor) //설정자
-	{
-		this.strProfessor = professor;
-	}
-	public static void main(String[] args)
-	{
+	public class Lecture {
 		
-		Scanner count = new Scanner(System.in);
-		System.out.print("수강하는 과목의 개수를 입력하시오: ");
-		int nameCount = count.nextInt();
+		//과목명, 담당교수
+		private String strName;            //과목명
+		private String strProfessor;       //담당교수
+		private static int putNum = 0;
 		
-		ArrayList<String> list1 = new ArrayList<>();
-		
-		for(int num = 0; num < nameCount; num++)
+		public Lecture(String name, String professor)  //생성자
 		{
-			Scanner putName = new Scanner(System.in);
-			System.out.print("과목명을 입력하시오: ");
-			String name = putName.next();
-
-			
-			Scanner putProfessor = new Scanner(System.in);
-			System.out.print("담당교수를 입력하시오: ");
-			String professor = putProfessor.next();
-			list1.add(name+" : "+professor);
+			this.strName = name;
+			this.strProfessor = professor;
+			this.putNum ++;
 		}
-		for(String i : list1) 
+		public int getLectureCount()
 		{
-			System.out.println(i);
+			return this.putNum;
 		}
-		
-
-	}
-
+		String getName() //접근자
+		{
+			return strName;
+		}
+		void setName(String name) //설정자
+		{
+			this.strName = name;
+		}
+		String getProfessor() //접근자
+		{
+			return strProfessor;
+		}
+		void setProfessor(String professor) //설정자
+		{
+			this.strProfessor = professor;
+		}
 }
---
+++ Student_Management_System/src/Ssh/Lecture_practice.java
@@ -0,0 +1,63 @@
+package Ssh;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lecture_practice {
+	//과목명, 담당교수
+	String strName;            //과목명
+	String strProfessor;       //담당교수
+	static int putNum = 0;
+	
+	public Lecture_practice(String name, String professor)  //생성자
+	{
+		this.strName = name;
+		this.strProfessor = professor;
+		this.putNum ++;
+	}
+	String getName() //접근자
+	{
+		return strName;
+	}
+	void setName(String name) //설정자
+	{
+		this.strName = name;
+	}
+	String getProfessor() //접근자
+	{
+		return strProfessor;
+	}
+	void setProfessor(String professor) //설정자
+	{
+		this.strProfessor = professor;
+	}
+	public static void main(String[] args)
+	{
+		
+		Scanner count = new Scanner(System.in);
+		System.out.print("수강하는 과목의 개수를 입력하시오: ");
+		int nameCount = count.nextInt();
+		
+		ArrayList<String> list1 = new ArrayList<>();
+		
+		for(int num = 0; num < nameCount; num++)
+		{
+			Scanner putName = new Scanner(System.in);
+			System.out.print("과목명을 입력하시오: ");
+			String name = putName.next();
+
+			
+			Scanner putProfessor = new Scanner(System.in);
+			System.out.print("담당교수를 입력하시오: ");
+			String professor = putProfessor.next();
+			list1.add(name+" : "+professor);
+		}
+		for(String i : list1) 
+		{
+			System.out.println(i);
+		}
+		System.out.println(putNum);
+
+	}
+
+}
