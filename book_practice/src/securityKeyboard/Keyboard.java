c9b34a052b52a676f7cb7b1199c768acaebb24db
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2020-10-28 19:57:53 +0900

    securityKeyboard_addFunction

--- book_practice/src/securityKeyboard/Keyboard.java
+++ book_practice/src/securityKeyboard/Keyboard.java
@@ -3,14 +3,17 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Collections;
-public class Keyboard extends JFrame{
+import java.util.Random;
+public class Keyboard extends JFrame implements ActionListener{
 	JTextField randomNum, inputNum;
 	JPanel north, center, south;
-	JButton[] number;
+	JButton[] number, blankB;
 	JButton blank, check;
-	int count = 0;
+	int count = 0, numIndex = 0;
 	
 	Keyboard(){
 		JFrame frame = new JFrame();
@@ -24,27 +27,42 @@ public class Keyboard extends JFrame{
 		center = new JPanel(new GridLayout(4, 4, 10, 10));
 		south = new JPanel(new GridLayout(1, 2, 10, 10));
 		
-		randomNum = new JTextField(30);
+		Font font = new Font("돋움체", 1, 20);
+		randomNum = new JTextField(20);
+		randomNum.setFont(font);
 		randomNum.setHorizontalAlignment(JTextField.CENTER);
-		inputNum = new JTextField("보안 문자를 입력하세요.",30);
+		randomNum.setText(numberGen());
+		inputNum = new JTextField("보안 문자를 입력하세요.",20);
 		inputNum.setHorizontalAlignment(JTextField.CENTER);
 		
-		String[] num = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "전체지움", "하나지움"};
+		String[] num = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "<html>전체<br>지움</html>", "<html>하나<br>지움</html>"};
 		Collections.shuffle(Arrays.asList(num));
-		int[] line = {0, 1, 2, 3};
-		number = new JButton[16];
-		
-		blank = new JButton("빈칸");
-		for(int i = 0; i < line.length; i ++)
-		{	
-			Collections.shuffle(Arrays.asList(line));
-			if()
-			number[i] = new JButton(num[i]);
-			
-		}
-		
+		String[] line = {"0", "1", "2", "3"};
+		Collections.shuffle(Arrays.asList(line));
+		number = new JButton[12];
+		blankB = new JButton[4];
 
+		for(int i = 0; i < line.length; i ++)
+		{
+			for(int j = 0; j < line.length; j ++)
+			{	
+				if(Integer.toString(j).equals(line[count]))
+				{
+					blankB[i] = new JButton("빈칸");
+					center.add(blankB[i]);
+				}
+				else 
+				{
+					number[numIndex] = new JButton(num[numIndex]);
+					center.add(number[numIndex]);
+					number[numIndex].addActionListener(this);
+					numIndex ++;
+				}
+			}
+			count++;
+		}	
 		check = new JButton("입력완료");
+		check.addActionListener(this);
 		south.add(new Panel());
 		south.add(check);
 		
@@ -55,6 +73,63 @@ public class Keyboard extends JFrame{
 		frame.add(center, BorderLayout.CENTER);
 		frame.add(south, BorderLayout.SOUTH);
 		frame.setVisible(true);
+	}
+	
+	
+	public String numberGen()
+	{
+		Random rand = new Random();
+		String numStr = "";
 		
+		for(int i = 0; i < 6; i ++)
+		{
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+		
+		return numStr;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		String buttonStr = e.getActionCommand();
+		if(inputNum.getText().equals("보안 문자를 입력하세요.") || inputNum.getText().equals("잘못 입력하였습니다.") || inputNum.getText().equals("잠금이 해제되었습니다."))
+		{
+			inputNum.setText("");
+			inputNum.setText(inputNum.getText() + buttonStr);
+		}
+
+		if(buttonStr == "<html>전체<br>지움</html>")
+		{
+			inputNum.setText("");
+		}
+		else if(buttonStr == "<html>하나<br>지움</html>")
+		{
+			if(inputNum.getText().equals("") || inputNum.getText().equals("보안 문자를 입력하세요.") || inputNum.getText().equals("잘못 입력하였습니다.") || inputNum.getText().equals("잠금이 해제되었습니다."))
+			{
+				inputNum.setText(inputNum.getText());
+			}
+			else
+			{
+				String nowNum = inputNum.getText();
+				inputNum.setText(nowNum.substring(0, nowNum.length()-1));
+			}
+		}
+		else if(buttonStr == "입력완료")
+		{
+			if(randomNum.getText().equals(inputNum.getText()))
+			{
+				inputNum.setText("잠금이 해제되었습니다.");
+			}
+			else
+			{
+				inputNum.setText("잘못 입력하였습니다.");
+			}
+		}
+		else
+		{
+			inputNum.setText(inputNum.getText() + buttonStr);
+		}	
 	}
 }
--- book_practice/src/securityKeyboard/Run.java
+++ book_practice/src/securityKeyboard/Run.java
@@ -1,14 +1,10 @@
 package securityKeyboard;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Run {
 
 	public static void main(String[] args) {
-		new Keyboard(); 
-		
+		new Keyboard();
+
 	}
 
 }
