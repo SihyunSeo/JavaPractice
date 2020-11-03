9fad2ac320893431abe0d84fef5166b789c404f0
SihyunSeo <63051137+SihyunSeo@users.noreply.github.com>
2020-11-03 12:25:12 +0900

    add_function

--- Project/src/New_gui/ServerRoot.java
+++ Project/src/New_gui/ServerRoot.java
@@ -1,57 +1,125 @@
 package New_gui;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 public class ServerRoot extends JFrame implements ActionListener{
-	JLabel folderRoot, mainFile;
-	JPanel folder, file, run, north;
-	JTextField folderTf, fileTf;
-	JButton folderB, fileB, runB;
-	ServerRoot()
-	{
-		setLayout(new BorderLayout());
-		setTitle("Server");
-		setSize(300, 150);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		north = new JPanel(new GridLayout(2,0));
-		folder = new JPanel();
-		file = new JPanel();
-		run = new JPanel(new GridLayout(1, 2));
-		
-		folderRoot = new JLabel("서버폴더");
-		mainFile = new JLabel("기본페이지");
-		
-		folderTf = new JTextField(15);
-		fileTf = new JTextField(14);
-		
-		folderB = new JButton("찾기");
-		fileB = new JButton("찾기");
-		runB = new JButton("실행");
-		
-		folder.add(folderRoot);
-		folder.add(folderTf);
-		folder.add(folderB);
-		
-		file.add(mainFile);
-		file.add(fileTf);
-		file.add(fileB);
-		
-		run.add(new JPanel());
-		run.add(runB);
-		
-		north.add(folder);
-		north.add(file);
-		
-		this.add(north, BorderLayout.NORTH);
-		this.add(run, BorderLayout.SOUTH);
-		setVisible(true);
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		
-	}
+   JLabel folderRoot, mainFile;
+   JPanel folder, file, run, north;
+   JTextField folderTf, fileTf;
+   JButton folderB, fileB, runB;
+   String folderPath = "", fileName = "";
+   ServerRoot()
+   {
+      setLayout(new BorderLayout());
+      setTitle("Server");
+      setSize(350, 150);
+      setResizable(false);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      north = new JPanel(new GridLayout(2,0));
+      folder = new JPanel();
+      file = new JPanel();
+      run = new JPanel(new GridLayout(1, 2));
+      
+      folderRoot = new JLabel("서버폴더");
+      mainFile = new JLabel("기본페이지");
+      
+      folderTf = new JTextField(15);
+      fileTf = new JTextField(14);
+      
+      folderB = new JButton("찾기");
+      fileB = new JButton("찾기");
+      runB = new JButton("실행");
+      
+      folderB.addActionListener(this);
+      fileB.addActionListener(this);
+      runB.addActionListener(this);
+      
+      folder.add(folderRoot);
+      folder.add(folderTf);
+      folder.add(folderB);
+      
+      file.add(mainFile);
+      file.add(fileTf);
+      file.add(fileB);
+      
+      run.add(new JPanel());
+      run.add(runB);
+      
+      north.add(folder);
+      north.add(file);
+      
+      this.add(north, BorderLayout.NORTH);
+      this.add(run, BorderLayout.SOUTH);
+      setVisible(true);
+   }
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      JButton b = (JButton)e.getSource();
+      if(e.getSource() == folderB)
+      {
+         fileRoot();
+      }
+      else if(e.getSource() == fileB)
+      {
+         mainFile();
+      }
+      else if(e.getSource() == runB)
+      {
+         folderTf.setText(folderPath);
+         fileTf.setText(fileName);
+         if(b.getText().equals("실행"))
+         {
+            b.setText("종료");
+         }
+         else if(b.getText().equals("종료"))
+         {
+            System.exit(0);
+         }
+      }
+   }
+   
+   public void fileRoot()
+   {
+      JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+      chooser.setCurrentDirectory(new File("/"));
+      chooser.setAcceptAllFileFilterUsed(true);
+      chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+      FileNameExtensionFilter filter = new FileNameExtensionFilter("Binary File", "cd11"); // filter 확장자 추가
+        chooser.setFileFilter(filter); // 파일 필터를 추가
+        int returnVal = chooser.showOpenDialog(null); // 열기용 창 오픈
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION) 
+        { // 열기를 클릭 
+            folderPath = chooser.getSelectedFile().toString();
+        }
+        else if(returnVal == JFileChooser.CANCEL_OPTION)
+        { // 취소를 클릭
+            folderPath = "";
+        }
+   }
+   public void mainFile()
+   {
+      JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+      chooser.setCurrentDirectory(new File("/"));
+      chooser.setAcceptAllFileFilterUsed(true);
+      chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+      FileNameExtensionFilter filter = new FileNameExtensionFilter("Binary File", "cd11"); // filter 확장자 추가
+        chooser.setFileFilter(filter); // 파일 필터를 추가
+        int returnVal = chooser.showOpenDialog(null); // 열기용 창 오픈
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION) 
+        { // 열기를 클릭 
+            fileName = chooser.getSelectedFile().getName();
+        }
+        else if(returnVal == JFileChooser.CANCEL_OPTION)
+        { // 취소를 클릭
+            fileName = "";
+        }
+   }
 }
